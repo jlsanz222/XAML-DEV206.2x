d02f9de03325eb31f2146486f0198be5ef8a6d12
jlsanz222 <jlsanzmerino@gmail.com>
2016-04-06 10:31:09 +0200

    Homework
    Homework 1

---
+++ RestaurantManager.Models/DataManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RestaurantManager.Models
+{
+    public abstract class DataManager : INotifyPropertyChanged
+    {
+        
+        protected RestaurantContext Repository { get;
+            private set ; }
+        
+
+        public DataManager()
+        {
+            LoadData();
+//            System.Diagnostics.Debug.WriteLine("OnDataLoaded DataManager");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([ CallerMemberName  ] string propName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+
+        private async void LoadData()
+        {
+           
+            this.Repository = new RestaurantContext();
+            await this.Repository.InitializeContextAsync();
+ //           System.Diagnostics.Debug.WriteLine("Antes LoadData DataManager");
+            OnDataLoaded();
+ //           System.Diagnostics.Debug.WriteLine("Despues LoadData DataManager");
+
+        }
+
+        protected abstract void OnDataLoaded();
+    }
+}
---
+++ RestaurantManager.Models/OrderDataManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+
+namespace RestaurantManager.Models
+{
+    public class OrderDataManager : DataManager
+    {       
+        protected override void OnDataLoaded()
+        {
+        //    System.Diagnostics.Debug.WriteLine("OnDataLoaded OrderDataManager");
+
+            this.MenuItems = Repository.StandardMenuItems;
+
+            OnPropertyChanged("MenuItems");
+
+        //    System.Diagnostics.Debug.WriteLine("MenuItems 1 "+this.MenuItems[1].Title.ToString());
+
+            this.CurrentlySelectedMenuItems = new List<MenuItem>
+            {
+                this.MenuItems[3],
+                this.MenuItems[5]
+            };
+            OnPropertyChanged("CurrentlySelectedMenuItems");
+
+        }
+               
+
+        public List<MenuItem> MenuItems { get;  set; }
+
+        public List<MenuItem> CurrentlySelectedMenuItems { get; set; }
+
+
+
+    }
+}
