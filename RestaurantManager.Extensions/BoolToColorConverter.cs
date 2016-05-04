536a7d8fab705d47f9563bb5dcb245649808df60
jlsanz222 <jlsanzmerino@gmail.com>
2016-05-04 21:41:58 +0200

    Homework 3
    Homework 3

---
+++ RestaurantManager.Extensions/BoolToColorConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI;
+
+
+namespace RestaurantManager.Extensions
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public Color TrueColor { get; set; }
+        public Color FalseColor { get; set; }
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            
+                if (value is bool) return (bool)value ? TrueColor : FalseColor;
+                return FalseColor;
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            bool returnValue = default(bool);
+            if (value is Color)
+            {
+                if ((Color)value == TrueColor) { returnValue = false; }
+                else if ((Color)value == FalseColor) { returnValue = true; }
+            }
+            return returnValue;
+        }
+    }
+}
---
+++ RestaurantManager.Extensions/RightClickWarnBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Popups;
+
+
+namespace RestaurantManager.Extensions
+{
+    public class RightClickWarnBehavior : DependencyObject, IBehavior { 
+    
+    public string Message { get; set; }
+    public string Title { get; set; }
+
+    public DependencyObject AssociatedObject { get; private set; }
+
+    private async void Page_RightTapped(object sender, RightTappedRoutedEventArgs e)
+    {
+        await new MessageDialog(this.Message, this.Title).ShowAsync();
+    }
+
+    public void Attach(DependencyObject associatedObject)
+    {
+        if (associatedObject is Page)
+        {
+            this.AssociatedObject = associatedObject;
+            (this.AssociatedObject as Page).RightTapped += Page_RightTapped;
+        }
+    }
+
+    public void Detach()
+    {
+        if (this.AssociatedObject != null && this.AssociatedObject is Page)
+        {
+            this.AssociatedObject = null;
+            (this.AssociatedObject as Page).RightTapped -= Page_RightTapped;
+        }
+    }
+        
+  }
+}
